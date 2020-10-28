525bb3238af2b53a940c91da44679bf063362cb9
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-10-27 17:10:22 -0700

    added CSS and title filter

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import './App.css';
 export default class App extends Component {
 
   state = {
+    keyword: '',
     horns: 0,
-    keyword: ''
+    title: ''
   }
 
   
@@ -24,6 +25,12 @@ export default class App extends Component {
     });
   }
 
+  handleTitleChange = e => {
+    this.setState({
+      title: e.target.value
+    });
+  }
+
   render() {
     const filteredCreatures = images.filter((creature) => {
       if (!this.state.keyword) return true;
@@ -34,47 +41,74 @@ export default class App extends Component {
       if (!this.state.horns) return true;
       if (creature.horns === +this.state.horns) return true;
       return false
+  })
+    .filter((creature) => {
+      if (!this.state.title) return true;
+      if (creature.title === this.state.title) return true;
+      return false
     })
 
+
     return (
-      <div>
-      <div>
-        <select onChange={this.handleKeywordChange}>
-          <option value=''>Filter by Keyword</option>
-          <option value='narwhal'>Narwhal</option>
-          <option value='rhino'>Rhino</option>
-          <option value='unicorn'>Unicorn</option>
-          <option value='unilego'>UniLego</option>
-          <option value='triceratops'>Triceratops</option>
-          <option value='markhor'>Markhor</option>
-          <option value='mouflon'>Mouflon</option>
-          <option value='addax'>Addax</option>
-          <option value='chameleon'>Chameleon</option>
-          <option value='lizard'> Lizard</option>
-          <option value='dragon'>Dragon</option>
-        </select>
-      </div>
-
-      <div>
-        <select onChange={this.handleHornChange}>
-          <option value=''>Filter by Horns</option>
-          <option value='1'>One</option>
-          <option value='2'>Two</option>
-          <option value='3'>Three</option>
-          <option value='100'>One Hundred</option>
-        </select>
-      </div>
-
-      <div className="App">
+      <div className="body">
         <Header />
-        {
-          filteredCreatures.map((images, i) =>
-        <ImageList
-          key={i}
-          images={images} />
-          )}
+        <div className="drop-downs">
+          <select className="keyword" onChange={this.handleKeywordChange}>
+            <option value=''>Filter by Keyword</option>
+            <option value='narwhal'>Narwhal</option>
+            <option value='rhino'>Rhino</option>
+            <option value='unicorn'>Unicorn</option>
+            <option value='unilego'>UniLego</option>
+            <option value='triceratops'>Triceratops</option>
+            <option value='markhor'>Markhor</option>
+            <option value='mouflon'>Mouflon</option>
+            <option value='addax'>Addax</option>
+            <option value='chameleon'>Chameleon</option>
+            <option value='lizard'> Lizard</option>
+            <option value='dragon'>Dragon</option>
+          </select>
+
+          <select className="horns" onChange={this.handleHornChange}>
+            <option value=''>Filter by Horns</option>
+            <option value='1'>One</option>
+            <option value='2'>Two</option>
+            <option value='3'>Three</option>
+            <option value='100'>One Hundred</option>
+          </select>
+
+          <select className="title" onChange={this.handleTitleChange}>
+            <option value=''>Filter by Title</option>
+            <option value='UniWhal'>UniWhal</option>
+            <option value='Rhino Family'>Rhino Family</option>
+            <option value='Unicorn Head'>Unicorn Head</option>
+            <option value='UniLego'>UniLego</option>
+            <option value='Basically a unicorn'>Basically A Unicorn</option>
+            <option value='#truth'>#truth</option>
+            <option value='Baby Rhino'>Baby Rhino</option>
+            <option value='Cera'>Cera</option>
+            <option value='Narwhal costume'>Narwhal Costume</option>
+            <option value='Rhino costume'> Rhino Costume</option>
+            <option value='Believe'>Believe</option>
+            <option value='Markhor'>Markhor</option>
+            <option value='Baby markhor'>Baby Markhor</option>
+            <option value='Mouflon'>Mouflon</option>
+            <option value='Addax'>Addax</option>
+            <option value='Baby mouflon'>Baby mouflon</option>
+            <option value="Happy Jackson's Chameleon">Happy Jackson's Chameleon</option>
+            <option value='This one is very serious'>Serious Jackson's Chameleon</option>
+            <option value='Horned Lizard'>Horned Lizard</option>
+            <option value='Smaug'>Smaug</option>
+          </select>
+        </div>
+        <div className="creature-display">
+          {
+            filteredCreatures.map((images, i) =>
+          <ImageList
+            key={i}
+            images={images} />
+            )}
+        </div>
       </div>
-    </div>
     )
   }
 }
--- src/ImageItem.js
+++ src/ImageItem.js
@@ -4,10 +4,10 @@ export default class ImageItem extends Component {
     render() {
         return (
             <div className={this.props.keyword}>
-                <img className="creature-image" src={this.props.url} alt="creature" />
+                <img className="creature-image" src={this.props.url} alt={this.props.description} />
                 <h3>{this.props.title}</h3>
-                <p>{this.props.description}</p>
-                <p>Number of Horns: {this.props.horns}</p>
+                <p className="d-text">{this.props.description}</p>
+                <p className="h-text">Number of Horns: {this.props.horns}</p>
             </div>
         )
     }
